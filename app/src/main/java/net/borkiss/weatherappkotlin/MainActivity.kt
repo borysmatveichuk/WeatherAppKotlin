c30a661c317582da4bc368f6278f78a67635c596
borysmatveichuk <boris.matveychuck@gmail.com>
2017-09-01 00:47:13 +0300

    - init commit

---
+++ app/src/main/java/net/borkiss/weatherappkotlin/MainActivity.kt
@@ -0,0 +1,26 @@
+package net.borkiss.weatherappkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+private val items = listOf(
+    "Mon 6/23 - Sunny - 31/17",
+    "Tue 6/24 - Foggy - 21/8",
+    "Wed 6/25 - Cloudy - 22/17",
+    "Thurs 6/26 - Rainy - 18/11",
+    "Fri 6/27 - Foggy - 21/10",
+    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+    "Sun 6/29 - Sunny - 20/7"
+)
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        forecast_list.layoutManager = LinearLayoutManager(this)
+        forecast_list.adapter = ForecastListAdapter(items)
+    }
+}
