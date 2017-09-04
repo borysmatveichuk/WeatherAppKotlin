075cb9e64368ec9b95fc9466a5896c79dcc0d662
borysmatveichuk <boris.matveychuck@gmail.com>
2017-09-05 00:40:49 +0300

    - add icon to data classes

--- app/src/main/java/net/borkiss/weatherappkotlin/domain/model/DomainClasses.kt
+++ app/src/main/java/net/borkiss/weatherappkotlin/domain/model/DomainClasses.kt
@@ -2,9 +2,15 @@ package net.borkiss.weatherappkotlin.domain.model
 
 data class ForecastList(val city: String,
                         val country: String,
-                        val dailyForecast: List<Forecast>)
+                        private val dailyForecast: List<Forecast>) {
+
+    val size: Int get() = dailyForecast.size
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
 
 data class Forecast(val date: String,
                     val description: String,
                     val high: Int,
-                    val low: Int)
+                    val low: Int,
+                    val iconUrl: String)
