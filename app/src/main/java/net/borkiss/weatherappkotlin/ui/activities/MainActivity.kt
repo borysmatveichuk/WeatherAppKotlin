a2d48b1b85bbdc15e04d16e29264a341d59b6586
Borys.M <boris-m@ukr.net>
2017-09-05 12:15:51 +0300

    - refactoring. clean code

--- app/src/main/java/net/borkiss/weatherappkotlin/ui/activities/MainActivity.kt
+++ app/src/main/java/net/borkiss/weatherappkotlin/ui/activities/MainActivity.kt
@@ -1,16 +1,15 @@
 package net.borkiss.weatherappkotlin.ui.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-//import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.*
 import net.borkiss.weatherappkotlin.R
-import net.borkiss.weatherappkotlin.data.ForecastRequest
 import net.borkiss.weatherappkotlin.domain.commands.RequestForecastCommand
-import net.borkiss.weatherappkotlin.domain.model.Forecast
 import net.borkiss.weatherappkotlin.ui.adapters.ForecastListAdapter
-import org.jetbrains.anko.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 private val items = listOf(
     "Mon 6/23 - Sunny - 31/17",
@@ -27,7 +26,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = find<RecyclerView> (R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
 
         doAsync {
--- app/src/main/java/net/borkiss/weatherappkotlin/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/net/borkiss/weatherappkotlin/ui/adapters/ForecastListAdapter.kt
@@ -4,14 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_forecast.view.*
 import net.borkiss.weatherappkotlin.R
 import net.borkiss.weatherappkotlin.domain.model.Forecast
 import net.borkiss.weatherappkotlin.domain.model.ForecastList
 import net.borkiss.weatherappkotlin.ui.utils.ctx
-import org.jetbrains.anko.find
 
 class ForecastListAdapter(private val weekForecast: ForecastList,
                           private val itemClick: (Forecast) -> Unit) :
@@ -31,19 +29,13 @@ class ForecastListAdapter(private val weekForecast: ForecastList,
     class ViewHolder(view: View, private val itemClick: (Forecast) -> Unit) :
             RecyclerView.ViewHolder(view) {
 
-        private val iconView: ImageView = view.find(R.id.icon)
-        private val dateView: TextView = view.find(R.id.date)
-        private val descriptionView: TextView = view.find(R.id.description)
-        private val maxTemperatureView: TextView = view.find(R.id.maxTemperature)
-        private val minTemperatureView: TextView = view.find(R.id.minTemperature)
-
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "${high}º"
-                minTemperatureView.text = "${low}º"
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high}º"
+                itemView.minTemperature.text = "${low}º"
                 itemView.setOnClickListener { itemClick(this) }
             }
 
