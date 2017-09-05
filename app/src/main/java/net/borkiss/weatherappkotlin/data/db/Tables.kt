30461a6f96936dda1d4ae88af67d7a1b858211b4
borysmatveichuk <boris.matveychuck@gmail.com>
2017-09-06 00:14:14 +0300

    - add sqlite db

---
+++ app/src/main/java/net/borkiss/weatherappkotlin/data/db/Tables.kt
@@ -0,0 +1,19 @@
+package net.borkiss.weatherappkotlin.data.db
+
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "Country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
