b42b62958cb07623c22ab2921d09fff052de7e5c
Borys.M <boris-m@ukr.net>
2017-10-10 12:29:51 +0300

    - change on click message

--- app/src/main/java/net/borkiss/weatherappkotlin/ui/activities/MainActivity.kt
+++ app/src/main/java/net/borkiss/weatherappkotlin/ui/activities/MainActivity.kt
@@ -11,16 +11,6 @@ import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
-private val items = listOf(
-    "Mon 6/23 - Sunny - 31/17",
-    "Tue 6/24 - Foggy - 21/8",
-    "Wed 6/25 - Cloudy - 22/17",
-    "Thurs 6/26 - Rainy - 18/11",
-    "Fri 6/27 - Foggy - 21/10",
-    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-    "Sun 6/29 - Sunny - 20/7"
-)
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +22,7 @@ class MainActivity : AppCompatActivity() {
             val result = RequestForecastCommand(1000).execute()
             uiThread {
                 forecastList.adapter = ForecastListAdapter(result,
-                        { toast(it.date.toString()) })
+                        { toast(it.description) })
             }
         }
     }
